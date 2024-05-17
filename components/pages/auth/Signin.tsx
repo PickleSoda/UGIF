6ec9ecde8550445a937187d4d38397fc916e6e2e
Dimitri Zhorzholiani <dmtzhorzholiani@gmail.com>
2024-05-17 23:49:56 +0400

    Auth redesign:

--- components/pages/auth/Signin.tsx
+++ components/pages/auth/Signin.tsx
@@ -11,6 +11,7 @@ import {
   IonIcon,
   IonInput,
   useIonRouter,
+  IonImg,
   useIonLoading,
   IonHeader,
 } from '@ionic/react';
@@ -56,7 +57,7 @@ const SignIn = () => {
     >
       <path
         d="M23 0L23.823 3.36707C25.109 8.62855 25.752 11.2593 27.1233 13.3821C28.336 15.2593 29.9527 16.8418 31.8554 18.0139C34.0071 19.3395 36.651 19.926 41.9388 21.0991L46 22L41.9388 22.9009C36.651 24.074 34.0071 24.6605 31.8554 25.9861C29.9527 27.1582 28.336 28.7407 27.1233 30.6179C25.752 32.7407 25.109 35.3714 23.823 40.6329L23 44L22.177 40.6329C20.891 35.3714 20.248 32.7407 18.8767 30.6179C17.664 28.7407 16.0473 27.1582 14.1446 25.9861C11.9929 24.6605 9.34898 24.074 4.06116 22.9009L0 22L4.06116 21.0991C9.34897 19.926 11.9929 19.3395 14.1446 18.0139C16.0473 16.8418 17.664 15.2593 18.8767 13.3821C20.248 11.2593 20.891 8.62855 22.177 3.36707L23 0Z"
-        fill="black"
+        fill="white"
       />
     </svg>
   );
@@ -142,7 +143,7 @@ const SignIn = () => {
   
   return (
     <IonPage>
-      <IonHeader mode='ios'>
+      <IonHeader mode='ios' className='container'>
         <IonToolbar className="custom-toolbar">
           <div className="star-svg-container">{star_svg}</div>
         </IonToolbar>
@@ -152,6 +153,8 @@ const SignIn = () => {
         scrollY={false}
         className="bg-login flex flex-col items-center justify-center"
       >
+        <IonImg src='rect-log.png' className="custom-logo" />
+
         <h1 className="login-title">Log in</h1>
 
         <div className="input-container">
@@ -213,6 +216,8 @@ const SignIn = () => {
             <IonIcon className="pl-1" slot="end" icon={logoGoogle}></IonIcon>
           </IonButton>
         </div>
+
+        <IonImg src='rect-log2.png' className="custom-logo-bottom" />
 
         <p className={`register-link ${keyboardVisible ? 'hidden' : ''}`}>
           Don&apos;t have an account?{' '}
